b9b6369c7bb60a43736fcfdbd749320de74716cb
Logan Shockley <loganv182@gmail.com>
2023-02-25 00:16:57 -0600

    Moved instant grab check

--- Core/src/Representation/PlayerRep.cs
+++ Core/src/Representation/PlayerRep.cs
@@ -228,14 +228,8 @@ namespace LabFusion.Representation
                 if (grip.IsInteractionDisabled || (grip.HasHost && grip.Host.IsInteractionDisabled))
                     return;
 
-                // Check for instantanious grabbing
-                bool isInstant = true;
-
-                if (grip.IsStatic || grip.TryCast<WorldGrip>() || grip.TryCast<GenericGrip>() || grip.TryCast<BarrelGrip>() || grip.TryCast<BoxGrip>())
-                    isInstant = false;
-
                 // Attach the hand
-                grip.TryAttach(hand, isInstant);
+                grip.TryAttach(hand, grip.CheckInstantAttach());
             }
         }
 
