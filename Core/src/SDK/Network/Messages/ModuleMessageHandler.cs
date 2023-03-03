feec1fe69fae11597fe6779f28089f91c93d70e6
Logan Shockley <loganv182@gmail.com>
2023-03-02 22:24:57 -0600

    Forgot to save a file

--- Core/src/SDK/Network/Messages/ModuleMessageHandler.cs
+++ Core/src/SDK/Network/Messages/ModuleMessageHandler.cs
@@ -60,7 +60,7 @@ namespace LabFusion.Network
             }
 
             // Return the byte pool
-            BytePool.Return(bytes);
+            ByteRetriever.Return(bytes);
         }
 
         public abstract void HandleMessage(byte[] bytes, bool isServerHandled = false);
@@ -140,7 +140,7 @@ namespace LabFusion.Network
             try
             {
                 ushort tag = BitConverter.ToUInt16(bytes, 0);
-                byte[] buffer = BytePool.Rent(bytes.Length - 2);
+                byte[] buffer = ByteRetriever.Rent(bytes.Length - 2);
 
                 for (var i = 0; i < buffer.Length; i++)
                     buffer[i] = bytes[i + 2];
