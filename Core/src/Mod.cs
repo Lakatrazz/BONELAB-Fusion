44ebfe9cc3cefd97aac937372966f66696d334cb
Logan Shockley <loganv182@gmail.com>
2023-03-13 15:59:28 -0500

    Made the release build of fusion.

--- Core/src/Mod.cs
+++ Core/src/Mod.cs
@@ -29,9 +29,9 @@ namespace LabFusion
 {
     public struct FusionVersion
     {
-        public const byte versionMajor = 0;
+        public const byte versionMajor = 1;
         public const byte versionMinor = 0;
-        public const short versionPatch = 1;
+        public const short versionPatch = 0;
     }
 
     public class FusionMod : MelonMod {
