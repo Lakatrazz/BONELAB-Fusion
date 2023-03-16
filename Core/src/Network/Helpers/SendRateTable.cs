8cb296ba9aa0748bf1499f98acb077aefd72258f
Logan Shockley <loganv182@gmail.com>
2023-03-16 00:59:04 -0500

    Replaced send rates with arrays

--- Core/src/Network/Helpers/SendRateTable.cs
+++ Core/src/Network/Helpers/SendRateTable.cs
@@ -7,63 +7,57 @@ using System.Threading.Tasks;
 
 namespace LabFusion.Network {
     public static class SendRateTable {
+        private static readonly int[] _playerTable = new int[9] {
+            2,
+            5,
+            10,
+            15,
+            30,
+            50,
+            70,
+            90,
+            130,
+        };
+
+        private static readonly int[] _objectTable = new int[14] {
+            700,
+            900,
+            1000,
+            1200,
+            1400,
+            1700,
+            2000,
+            2500,
+            2700,
+            3000,
+            3200,
+            3400,
+            3700,
+            4200,
+        };
+
         public static int GetPlayerSendRate() {
             var count = PlayerIdManager.PlayerCount;
+            var tableLength = _playerTable.Length;
 
-            if (count <= 2)
-                return 1;
-            else if (count <= 5)
-                return 2;
-            else if (count <= 10)
-                return 3;
-            else if (count <= 15)
-                return 4;
-            else if (count <= 30)
-                return 5;
-            else if (count <= 50)
-                return 6;
-            else if (count <= 70)
-                return 7;
-            else if (count <= 90)
-                return 8;
-            else if (count <= 130)
-                return 9;
-            else
-                return 10;
+            for (var i = 0; i < tableLength; i++) {
+                if (count <= _playerTable[i])
+                    return i + 1;
+            }
+
+            return tableLength + 1;
         }
 
         public static int GetObjectSendRate(int bytesUp)
         {
-            if (bytesUp <= 700)
-                return 1;
-            else if (bytesUp <= 900)
-                return 2;
-            else if (bytesUp <= 1000)
-                return 3;
-            else if (bytesUp <= 1200)
-                return 4;
-            else if (bytesUp <= 1400)
-                return 5;
-            else if (bytesUp <= 1700)
-                return 6;
-            else if (bytesUp <= 2000)
-                return 7;
-            else if (bytesUp <= 2500)
-                return 8;
-            else if (bytesUp <= 2700)
-                return 9;
-            else if (bytesUp <= 3000)
-                return 10;
-            else if (bytesUp <= 3200)
-                return 11;
-            else if (bytesUp <= 3400)
-                return 12;
-            else if (bytesUp <= 3700)
-                return 13;
-            else if (bytesUp <= 4200)
-                return 14;
-            else
-                return 15;
+            var tableLength = _objectTable.Length;
+
+            for (var i = 0; i < tableLength; i++) {
+                if (bytesUp <= _objectTable[i])
+                    return i + 1;
+            }
+
+            return tableLength + 1;
         }
 
     }
