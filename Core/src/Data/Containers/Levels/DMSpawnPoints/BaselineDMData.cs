afd3c9e7a2e2431eb889cc4833204482fb351a7e
intinvest <clarkxlu@gmail.com>
2023-07-19 18:46:53 -0700

    Hopefully fixed moonbase, added Baseline, Magma Gate, and Rooftops

---
+++ Core/src/Data/Containers/Levels/DMSpawnPoints/BaselineDMData.cs
@@ -0,0 +1,36 @@
+﻿using LabFusion.MarrowIntegration;
+using LabFusion.Utilities;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityEngine;
+
+namespace LabFusion.Data {
+    public class BaselineDMData : LevelDataHandler {
+        private static readonly Vector3[] _deathmatchSpawnPoints = new Vector3[5] {
+    new Vector3(-38.7547f, 0.0375f, 38.9284f),
+    new Vector3(38.0896f, 0.0374f, 38.1792f),
+    new Vector3(35.7948f, -1.9619f, -45.7488f),
+    new Vector3(-38.8625f, 0.0377f, -38.8162f),
+    new Vector3(-2.0933f, 0.0374f, -3.9311f),
+        };
+
+        protected override void MainSceneInitialized() {
+            // Check if this is the right map
+            if (FusionSceneManager.Title == "Baseline" && FusionSceneManager.Level.Pallet.Internal) {
+                // Create DM spawn points
+                for (var i = 0; i < _deathmatchSpawnPoints.Length; i++) {
+                    GameObject spawnPoint = new GameObject("Deathmatch Spawn");
+                    spawnPoint.transform.position = _deathmatchSpawnPoints[i];
+                    spawnPoint.AddComponent<DeathmatchSpawnpoint>();
+                    spawnPoint.AddComponent<LavaGangSpawnpoint>();
+                    spawnPoint.AddComponent<SabrelakeSpawnpoint>();
+                }
+            }
+        }
+    }
+}
---
+++ Core/src/Data/Containers/Levels/DMSpawnPoints/RooftopsDMData.cs
@@ -0,0 +1,45 @@
+﻿using LabFusion.MarrowIntegration;
+using LabFusion.Utilities;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityEngine;
+
+namespace LabFusion.Data {
+    public class RooftopsDMData : LevelDataHandler {
+        private static readonly Vector3[] _deathmatchSpawnPoints = new Vector3[14] {
+    new Vector3(59.4848f, 93.0314f, -54.4698f),
+    new Vector3(74.7662f, 90.0374f, -57.4251f),
+    new Vector3(60.3601f, 92.2162f, -69.01f),
+    new Vector3(53.8805f, 90.197f, -66.1647f),
+    new Vector3(42.8871f, 90.197f, -66.5409f),
+    new Vector3(35.5215f, 90.7248f, -60.718f),
+    new Vector3(51.6421f, 93.698f, -55.6575f),
+    new Vector3(45.2663f, 79.6913f, -38.3841f),
+    new Vector3(59.7499f, 90.0374f, -53.5572f),
+    new Vector3(42.109f, 93.569f, -66.7509f),
+    new Vector3(37.153f, 90.0374f, -54.4797f),
+    new Vector3(37.6532f, 90.197f, -66.376f),
+    new Vector3(64.6106f, 93.5873f, -62.8519f),
+    new Vector3(69.8322f, 99.0641f, -68.1835f),
+        };
+
+        protected override void MainSceneInitialized() {
+            // Check if this is the right map
+            if (FusionSceneManager.Title == "Rooftops" && FusionSceneManager.Level.Pallet.Internal) {
+                // Create DM spawn points
+                for (var i = 0; i < _deathmatchSpawnPoints.Length; i++) {
+                    GameObject spawnPoint = new GameObject("Deathmatch Spawn");
+                    spawnPoint.transform.position = _deathmatchSpawnPoints[i];
+                    spawnPoint.AddComponent<DeathmatchSpawnpoint>();
+                    spawnPoint.AddComponent<LavaGangSpawnpoint>();
+                    spawnPoint.AddComponent<SabrelakeSpawnpoint>();
+                }
+            }
+        }
+    }
+}
