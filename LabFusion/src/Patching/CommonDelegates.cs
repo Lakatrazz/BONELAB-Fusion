57c50f4ead9f2faec674dabdc8a9198afc78c7c8
Logan Shockley <loganv182@gmail.com>
2024-06-10 13:35:01 -0500

    Removed useless patch delegates

--- LabFusion/src/Patching/CommonDelegates.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace LabFusion.Patching
-{
-    public delegate void ReceiveAttackPatchDelegate(IntPtr instance, IntPtr attack, IntPtr method);
-
-    public delegate void EmptyPatchDelegate(IntPtr instance, IntPtr method);
-}
