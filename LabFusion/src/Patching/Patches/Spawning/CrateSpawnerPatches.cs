1c6dfa92d3eaebb2d5b08fb3beb3183233bf1fac
Logan Shockley <loganv182@gmail.com>
2024-06-13 23:51:47 -0500

    WIP crate spawner networking

--- LabFusion/src/Patching/Patches/Spawning/CrateSpawnerPatches.cs
+++ LabFusion/src/Patching/Patches/Spawning/CrateSpawnerPatches.cs
@@ -1,10 +1,11 @@
 ﻿using HarmonyLib;
 
 using LabFusion.Network;
-using LabFusion.Senders;
+using LabFusion.RPC;
+using LabFusion.Utilities;
 
-using Il2CppSLZ.Marrow.Pool;
 using Il2CppSLZ.Marrow.Warehouse;
+
 using UnityEngine;
 
 namespace LabFusion.Patching
@@ -13,33 +14,44 @@ namespace LabFusion.Patching
     public static class CrateSpawnerPatches
     {
         [HarmonyPrefix]
-        [HarmonyPatch(nameof(CrateSpawner.Awake))]
-        public static void Awake(CrateSpawner __instance)
+        [HarmonyPatch(nameof(CrateSpawner.SpawnSpawnable))]
+        public static bool SpawnSpawnable(CrateSpawner __instance)
         {
-            var action = (Il2CppSystem.Action<CrateSpawner, GameObject>)((c, g) => { Internal_OnPlace(__instance); });
+            // If there is NO server, the spawner can function as normal.
+            if (!NetworkInfo.HasServer)
+            {
+                return true;
+            }
 
-            if (__instance.onSpawnEvent != null)
-                __instance.onSpawnEvent.add_DynamicCalls(action);
+            // If we aren't the server, don't allow a crate spawn
+            if (!NetworkInfo.IsServer)
+            {
+                return false;
+            }
+
+            // Otherwise, manually sync this spawn over the network
+            var spawnable = __instance._spawnable;
+            var transform = __instance.transform;
+
+            NetworkAssetSpawner.Spawn(new NetworkAssetSpawner.SpawnRequestInfo()
+            {
+                spawnable = spawnable,
+                position = transform.position,
+                rotation = transform.rotation,
+                spawnCallback = (info) =>
+                {
+                    OnNetworkSpawn(__instance, info);
+                },
+            });
+
+            return false;
         }
 
-        private static void Internal_OnPlace(CrateSpawner scp)
+        private static void OnNetworkSpawn(CrateSpawner spawner, NetworkAssetSpawner.SpawnCallbackInfo info)
         {
-            // Get the last spawned object
-            // placedSpawnable is always null
-            if (NetworkInfo.IsServer && scp.spawnableCrateReference.Crate != null)
-            {
-                var crate = scp.spawnableCrateReference.Crate;
+            var spawned = info.spawned;
 
-                var barcodeToPool = AssetSpawner._instance._barcodeToPool;
-
-                if (barcodeToPool.ContainsKey(crate.Barcode))
-                {
-                    var pool = barcodeToPool[crate.Barcode];
-
-                    var lastSpawned = pool._spawned[pool._spawned.Count - 1];
-                    SpawnSender.SendCratePlacerEvent(scp, lastSpawned.gameObject);
-                }
-            }
+            spawner.OnPooleeSpawn(spawned);
         }
     }
 }
