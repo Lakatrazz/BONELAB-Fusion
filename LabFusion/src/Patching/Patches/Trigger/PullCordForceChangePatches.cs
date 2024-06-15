bb644067444715e09913959582b9f7f9aa5edc5d
Logan Shockley <loganv182@gmail.com>
2024-06-14 21:53:17 -0500

    Fixed pull cord force change patch

--- LabFusion/src/Patching/Patches/Trigger/PullCordForceChangePatches.cs
+++ LabFusion/src/Patching/Patches/Trigger/PullCordForceChangePatches.cs
@@ -4,6 +4,8 @@ using LabFusion.Network;
 using LabFusion.Utilities;
 
 using Il2CppSLZ.Bonelab;
+using Il2CppSLZ.Marrow.Interaction;
+using Il2CppSLZ.Rig;
 
 using UnityEngine;
 
@@ -16,9 +18,31 @@ namespace LabFusion.Patching
         [HarmonyPatch(nameof(PullCordForceChange.OnTriggerEnter))]
         public static bool OnTriggerEnter(Collider other)
         {
-            if (NetworkInfo.HasServer && other.CompareTag("Player"))
+            if (!NetworkInfo.HasServer)
             {
-                return TriggerUtilities.IsMainRig(other);
+                return true;
+            }
+
+            var rigidbody = other.attachedRigidbody;
+
+            if (rigidbody == null)
+            {
+                return true;
+            }
+
+            var marrowBody = MarrowBody.Cache.Get(rigidbody.gameObject);
+
+            if (marrowBody == null)
+            {
+                return true;
+            }
+
+            var marrowEntity = marrowBody.Entity;
+            var rigManager = RigManager.Cache.Get(marrowEntity.gameObject);
+
+            if (rigManager != null)
+            {
+                return rigManager.IsSelf();
             }
 
             return true;
