9c363f3d7b3624a3fd7b2cab3ddb32762136caed
Logan Shockley <loganv182@gmail.com>
2024-06-14 23:40:54 -0500

    Moved puppet master kill detection patch

--- LabFusion/src/Patching/Patches/PuppetMasterPatches.cs
+++ LabFusion/src/Patching/Patches/PuppetMasterPatches.cs
@@ -13,36 +13,35 @@ namespace LabFusion.Patching
     {
         public static bool IgnorePatches = false;
 
-        [HarmonyPatch(nameof(PuppetMaster.Kill))]
+        [HarmonyPatch(nameof(PuppetMaster.PostKill))]
         [HarmonyPrefix]
-        [HarmonyPatch(new Type[0])]
-        public static bool Kill(PuppetMaster __instance)
+        public static void PostKill(PuppetMaster __instance)
         {
             if (IgnorePatches)
-                return true;
-
-            if (NetworkInfo.HasServer && PuppetMasterExtender.Cache.TryGet(__instance, out var syncable))
             {
-                if (!syncable.IsOwner())
-                    return false;
-                else
-                {
-                    using (var writer = FusionWriter.Create(PropReferenceData.Size))
-                    {
-                        var data = PropReferenceData.Create(PlayerIdManager.LocalSmallId, syncable.Id);
-                        writer.Write(data);
-
-                        using var message = FusionMessage.Create(NativeMessageTag.PuppetMasterKill, writer);
-                        MessageSender.SendToServer(NetworkChannel.Reliable, message);
-                    }
-
-                    PuppetMasterExtender.LastKilled = syncable;
-
-                    return true;
-                }
+                return;
             }
 
-            return true;
+            if (!NetworkInfo.HasServer)
+            {
+                return;
+            }
+
+            if (!PuppetMasterExtender.Cache.TryGet(__instance, out var syncable) || !syncable.IsOwner())
+            {
+                return;
+            }
+
+            using (var writer = FusionWriter.Create(PropReferenceData.Size))
+            {
+                var data = PropReferenceData.Create(PlayerIdManager.LocalSmallId, syncable.Id);
+                writer.Write(data);
+
+                using var message = FusionMessage.Create(NativeMessageTag.PuppetMasterKill, writer);
+                MessageSender.SendToServer(NetworkChannel.Reliable, message);
+            }
+
+            PuppetMasterExtender.LastKilled = syncable;
         }
     }
 }
