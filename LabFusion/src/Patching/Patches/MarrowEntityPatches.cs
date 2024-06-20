8880d675a573bec95dfec859652a68d543d764eb
Logan Shockley <loganv182@gmail.com>
2024-06-20 14:38:35 -0500

    Changed cull apply patch

--- LabFusion/src/Patching/Patches/MarrowEntityPatches.cs
+++ LabFusion/src/Patching/Patches/MarrowEntityPatches.cs
@@ -18,8 +18,8 @@ namespace LabFusion.Patching;
 public static class MarrowEntityPatches
 {
     [HarmonyPostfix]
-    [HarmonyPatch(nameof(MarrowEntity.OnCullResolve))]
-    public static void OnCullResolve(MarrowEntity __instance, InactiveStatus status, bool isInactive)
+    [HarmonyPatch(nameof(MarrowEntity.OnCullApply))]
+    public static void OnCullApply(MarrowEntity __instance, InactiveStatus status, bool isInactive)
     {
         if (!NetworkInfo.HasServer)
         {
