f65e6f100429b8e5f02bcb12a1e0324e77ab63a0
Logan Shockley <loganv182@gmail.com>
2024-06-20 15:59:06 -0500

    Added grip unculling

--- LabFusion/src/Entities/Player/RigGrabber.cs
+++ LabFusion/src/Entities/Player/RigGrabber.cs
@@ -5,19 +5,25 @@ using Il2CppSLZ.Marrow.Utilities;
 using LabFusion.Data;
 using LabFusion.Extensions;
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using MelonLoader;
+
+using System.Collections;
 
 namespace LabFusion.Entities;
 
 public class RigGrabber
 {
+    public class GrabberData
+    {
+        public Grip grip;
+        public SimpleTransform? targetInBase = null;
+    }
+
     private RigReferenceCollection _references = null;
 
     private bool _isCulled = false;
+
+    private FusionDictionary<Handedness, GrabberData> _lastGrabs = new();
 
     public RigGrabber(RigReferenceCollection references)
     {
@@ -30,13 +36,34 @@ public class RigGrabber
 
         if (isInactive)
         {
-            Detach(Handedness.LEFT);
-            Detach(Handedness.RIGHT);
+            DetachWithoutClear(Handedness.LEFT);
+            DetachWithoutClear(Handedness.RIGHT);
         }
+        else
+        {
+            MelonCoroutines.Start(WaitAndUncullGrips());
+        }
+    }
+
+    private IEnumerator WaitAndUncullGrips()
+    {
+        for (var i = 0; i < 120; i++)
+        {
+            yield return null;
+        }
+
+        UncullGrip(Handedness.LEFT);
+        UncullGrip(Handedness.RIGHT);
     }
 
     public void Attach(Handedness handedness, Grip grip, SimpleTransform? targetInBase = null)
     {
+        _lastGrabs[handedness] = new GrabberData()
+        {
+            grip = grip,
+            targetInBase = targetInBase
+        };
+
         if (_isCulled)
         {
             return;
@@ -65,6 +92,13 @@ public class RigGrabber
 
     public void Detach(Handedness handedness)
     {
+        _lastGrabs.Remove(handedness);
+
+        DetachWithoutClear(handedness);
+    }
+
+    private void DetachWithoutClear(Handedness handedness)
+    {
         var hand = _references.GetHand(handedness);
 
         if (hand == null)
@@ -73,5 +107,21 @@ public class RigGrabber
         }
 
         hand.TryDetach();
+    }
+
+    private void UncullGrip(Handedness handedness)
+    {
+        if (!_lastGrabs.TryGetValue(handedness, out var data))
+        {
+            return;
+        }
+
+        if (data.grip.IsNOC())
+        {
+            _lastGrabs.Remove(handedness);
+            return;
+        }
+
+        Attach(handedness, data.grip, data.targetInBase);
     }
 }
