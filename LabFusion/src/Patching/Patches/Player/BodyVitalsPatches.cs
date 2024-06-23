60dcd565efc72e0305fc8dcca32fa5a59e700e1d
Logan Shockley <loganv182@gmail.com>
2024-06-22 22:48:02 -0500

    Made UI rig not scale from other players

---
+++ LabFusion/src/Patching/Patches/Player/BodyVitalsPatches.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using HarmonyLib;
+
+using Il2CppSLZ.Bonelab;
+
+using LabFusion.Utilities;
+
+namespace LabFusion.Patching;
+
+[HarmonyPatch(typeof(BodyVitals))]
+public static class BodyVitalsPatches
+{
+    [HarmonyPrefix]
+    [HarmonyPatch(nameof(BodyVitals.ApplyUILayoutSettings))]
+    public static bool ApplyUILayoutSettings(BodyVitals __instance)
+    {
+        var rig = __instance.ctrl_Rig;
+        
+        // Don't scale the UI rig if this isn't the main RigManager
+        if (!rig.manager.IsSelf())
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
