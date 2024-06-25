c7cd3637741096b25377a9f2847f40089931d53b
Logan Shockley <loganv182@gmail.com>
2024-06-25 17:13:13 -0500

    Cleaned up a few things

--- LabFusion/src/Network/Messages/FusionMessage.cs
+++ LabFusion/src/Network/Messages/FusionMessage.cs
@@ -1,135 +1,134 @@
 ﻿using LabFusion.Utilities;
 using System.Runtime.InteropServices;
 
-namespace LabFusion.Network
+namespace LabFusion.Network;
+
+public enum NetworkChannel : byte
 {
-    public enum NetworkChannel : byte
+    Reliable,
+    Unreliable,
+    VoiceChat,
+}
+
+
+public unsafe class FusionMessage : IDisposable
+{
+    private byte* _buffer;
+    private int _size;
+
+    private bool _disposed;
+
+    public int Length
     {
-        Reliable,
-        Unreliable,
-        VoiceChat,
+        get
+        {
+            return _size;
+        }
     }
 
-
-    public unsafe class FusionMessage : IDisposable
+    public byte* Buffer
     {
-        private byte* _buffer;
-        private int _size;
-
-        private bool _disposed;
-
-        public int Length
+        get
         {
-            get
-            {
-                return _size;
-            }
+            return _buffer;
+        }
+    }
+
+    internal static FusionMessage Internal_Create(int size)
+    {
+        return new FusionMessage()
+        {
+            _buffer = (byte*)Marshal.AllocHGlobal(size),
+            _size = size,
+            _disposed = false,
+        };
+    }
+
+    public static FusionMessage Create(byte tag, FusionWriter writer)
+    {
+        return Create(tag, writer.Buffer, writer.Length);
+    }
+
+    public static FusionMessage Create(byte tag, byte[] buffer, int length = -1)
+    {
+        if (length <= 0)
+            length = buffer.Length;
+
+        int size = length + 1;
+        var message = Internal_Create(size);
+
+        message._buffer[0] = tag;
+        for (var i = 0; i < length; i++)
+        {
+            message._buffer[i + 1] = buffer[i];
         }
 
-        public byte* Buffer
-        {
-            get
-            {
-                return _buffer;
-            }
-        }
+        return message;
+    }
 
-        internal static FusionMessage Internal_Create(int size)
-        {
-            return new FusionMessage()
-            {
-                _buffer = (byte*)Marshal.AllocHGlobal(size),
-                _size = size,
-                _disposed = false,
-            };
-        }
+    public static FusionMessage ModuleCreate<TMessage>(FusionWriter writer) where TMessage : ModuleMessageHandler
+    {
+        return ModuleCreate(typeof(TMessage), writer);
+    }
 
-        public static FusionMessage Create(byte tag, FusionWriter writer)
-        {
-            return Create(tag, writer.Buffer, writer.Length);
-        }
+    public static FusionMessage ModuleCreate<TMessage>(byte[] buffer) where TMessage : ModuleMessageHandler
+    {
+        return ModuleCreate(typeof(TMessage), buffer);
+    }
 
-        public static FusionMessage Create(byte tag, byte[] buffer, int length = -1)
-        {
-            if (length <= 0)
-                length = buffer.Length;
+    public static FusionMessage ModuleCreate(Type type, FusionWriter writer)
+    {
+        return ModuleCreate(type, writer.Buffer, writer.Length);
+    }
 
-            int size = length + 1;
+    public static FusionMessage ModuleCreate(Type type, byte[] buffer, int length = -1)
+    {
+        if (length <= 0)
+            length = buffer.Length;
+
+        int size = length + 3;
+
+        // Assign the module type
+        var tag = ModuleMessageHandler.GetHandlerTag(type);
+
+        // Make sure the tag is valid, otherwise we dont return a message
+        if (tag.HasValue)
+        {
+            var value = tag.Value;
+            var tagBytes = BitConverter.GetBytes((ushort)value);
+
             var message = Internal_Create(size);
+            message._buffer[0] = NativeMessageTag.Module;
+            message._buffer[1] = tagBytes[0];
+            message._buffer[2] = tagBytes[1];
 
-            message._buffer[0] = tag;
             for (var i = 0; i < length; i++)
             {
-                message._buffer[i + 1] = buffer[i];
+                message._buffer[i + 3] = buffer[i];
             }
 
             return message;
         }
+        else
+            return null;
+    }
 
-        public static FusionMessage ModuleCreate<TMessage>(FusionWriter writer) where TMessage : ModuleMessageHandler
-        {
-            return ModuleCreate(typeof(TMessage), writer);
-        }
+    public byte[] ToByteArray()
+    {
+        byte[] bytes = ByteRetriever.Rent(Length);
+        Marshal.Copy((IntPtr)_buffer, bytes, 0, Length);
 
-        public static FusionMessage ModuleCreate<TMessage>(byte[] buffer) where TMessage : ModuleMessageHandler
-        {
-            return ModuleCreate(typeof(TMessage), buffer);
-        }
+        return bytes;
+    }
 
-        public static FusionMessage ModuleCreate(Type type, FusionWriter writer)
-        {
-            return ModuleCreate(type, writer.Buffer, writer.Length);
-        }
+    public void Dispose()
+    {
+        if (_disposed)
+            return;
 
-        public static FusionMessage ModuleCreate(Type type, byte[] buffer, int length = -1)
-        {
-            if (length <= 0)
-                length = buffer.Length;
+        GC.SuppressFinalize(this);
+        Marshal.FreeHGlobal((IntPtr)_buffer);
 
-            int size = length + 3;
-
-            // Assign the module type
-            var tag = ModuleMessageHandler.GetHandlerTag(type);
-
-            // Make sure the tag is valid, otherwise we dont return a message
-            if (tag.HasValue)
-            {
-                var value = tag.Value;
-                var tagBytes = BitConverter.GetBytes((ushort)value);
-
-                var message = Internal_Create(size);
-                message._buffer[0] = NativeMessageTag.Module;
-                message._buffer[1] = tagBytes[0];
-                message._buffer[2] = tagBytes[1];
-
-                for (var i = 0; i < length; i++)
-                {
-                    message._buffer[i + 3] = buffer[i];
-                }
-
-                return message;
-            }
-            else
-                return null;
-        }
-
-        public byte[] ToByteArray()
-        {
-            byte[] bytes = ByteRetriever.Rent(Length);
-            Marshal.Copy((IntPtr)_buffer, bytes, 0, Length);
-
-            return bytes;
-        }
-
-        public void Dispose()
-        {
-            if (_disposed)
-                return;
-
-            GC.SuppressFinalize(this);
-            Marshal.FreeHGlobal((IntPtr)_buffer);
-
-            _disposed = true;
-        }
+        _disposed = true;
     }
 }
--- LabFusion/src/Senders/Props/SpawnSender.cs
+++ LabFusion/src/Senders/Props/SpawnSender.cs
@@ -1,99 +1,96 @@
-﻿using UnityEngine;
-
-using LabFusion.Network;
+﻿using LabFusion.Network;
 using LabFusion.Data;
-using LabFusion.Syncables;
 using LabFusion.Utilities;
-using Il2CppSLZ.Marrow.Warehouse;
 using LabFusion.Entities;
 
-namespace LabFusion.Senders
-{
-    public static class SpawnSender
-    {
-        /// <summary>
-        /// Sends a catchup for the OnPlaceEvent for a CrateSpawner.
-        /// </summary>
-        /// <param name="placer"></param>
-        /// <param name="syncable"></param>
-        /// <param name="userId"></param>
-        public static void SendCratePlacerCatchup(CrateSpawner placer, NetworkEntity entity, ulong userId)
-        {
-            if (!NetworkInfo.IsServer)
-            {
-                return;
-            }
+using Il2CppSLZ.Marrow.Warehouse;
 
-            using var writer = FusionWriter.Create(CrateSpawnerData.Size);
-            var data = CrateSpawnerData.Create(entity.Id, placer.gameObject);
+namespace LabFusion.Senders;
+
+public static class SpawnSender
+{
+    /// <summary>
+    /// Sends a catchup for the OnPlaceEvent for a CrateSpawner.
+    /// </summary>
+    /// <param name="placer"></param>
+    /// <param name="syncable"></param>
+    /// <param name="userId"></param>
+    public static void SendCratePlacerCatchup(CrateSpawner placer, NetworkEntity entity, ulong userId)
+    {
+        if (!NetworkInfo.IsServer)
+        {
+            return;
+        }
+
+        using var writer = FusionWriter.Create(CrateSpawnerData.Size);
+        var data = CrateSpawnerData.Create(entity.Id, placer.gameObject);
+        writer.Write(data);
+
+        using var message = FusionMessage.Create(NativeMessageTag.CrateSpawner, writer);
+        MessageSender.SendFromServer(userId, NetworkChannel.Reliable, message);
+    }
+
+    /// <summary>
+    /// Sends the OnPlaceEvent for a CrateSpawner.
+    /// </summary>
+    /// <param name="placer"></param>
+    /// <param name="go"></param>
+    public static void SendCratePlacerEvent(CrateSpawner placer, ushort spawnedId)
+    {
+        if (!NetworkInfo.IsServer)
+        {
+            return;
+        }
+
+        // Wait for the level to load and for 5 frames before sending messages
+        FusionSceneManager.HookOnLevelLoad(() =>
+        {
+            DelayUtilities.Delay(() =>
+            {
+                Internal_OnSendCratePlacer(placer, spawnedId);
+            }, 5);
+        });
+    }
+
+    private static void Internal_OnSendCratePlacer(CrateSpawner placer, ushort spawnedId)
+    {
+        using (var writer = FusionWriter.Create(CrateSpawnerData.Size))
+        {
+            var data = CrateSpawnerData.Create(spawnedId, placer.gameObject);
             writer.Write(data);
 
             using var message = FusionMessage.Create(NativeMessageTag.CrateSpawner, writer);
-            MessageSender.SendFromServer(userId, NetworkChannel.Reliable, message);
+            MessageSender.BroadcastMessageExceptSelf(NetworkChannel.Reliable, message);
         }
 
-        /// <summary>
-        /// Sends the OnPlaceEvent for a CrateSpawner.
-        /// </summary>
-        /// <param name="placer"></param>
-        /// <param name="go"></param>
-        public static void SendCratePlacerEvent(CrateSpawner placer, ushort spawnedId)
+        // Insert the catchup hook for future users
+        var entity = NetworkEntityManager.IdManager.RegisteredEntities.GetEntity(spawnedId);
+
+        if (entity != null)
         {
-            if (!NetworkInfo.IsServer)
+            entity.OnEntityCatchup += (entity, player) =>
             {
-                return;
-            }
-
-            // Wait for the level to load and for 5 frames before sending messages
-            FusionSceneManager.HookOnLevelLoad(() =>
-            {
-                DelayUtilities.Delay(() =>
-                {
-                    Internal_OnSendCratePlacer(placer, spawnedId);
-                }, 5);
-            });
+                SendCratePlacerCatchup(placer, entity, player);
+            };
         }
+    }
 
-        private static void Internal_OnSendCratePlacer(CrateSpawner placer, ushort spawnedId)
+    /// <summary>
+    /// Sends a catchup sync message for a pool spawned object.
+    /// </summary>
+    /// <param name="syncable"></param>
+    public static void SendCatchupSpawn(byte owner, string barcode, ushort syncId, SerializedTransform serializedTransform, ulong userId)
+    {
+        if (!NetworkInfo.IsServer)
         {
-            using (var writer = FusionWriter.Create(CrateSpawnerData.Size))
-            {
-                var data = CrateSpawnerData.Create(spawnedId, placer.gameObject);
-                writer.Write(data);
-
-                using var message = FusionMessage.Create(NativeMessageTag.CrateSpawner, writer);
-                MessageSender.BroadcastMessageExceptSelf(NetworkChannel.Reliable, message);
-            }
-
-            // Insert the catchup hook for future users
-            var entity = NetworkEntityManager.IdManager.RegisteredEntities.GetEntity(spawnedId);
-
-            if (entity != null)
-            {
-                entity.OnEntityCatchup += (entity, player) =>
-                {
-                    SendCratePlacerCatchup(placer, entity, player);
-                };
-            }
+            return;
         }
 
-        /// <summary>
-        /// Sends a catchup sync message for a pool spawned object.
-        /// </summary>
-        /// <param name="syncable"></param>
-        public static void SendCatchupSpawn(byte owner, string barcode, ushort syncId, SerializedTransform serializedTransform, ulong userId)
-        {
-            if (!NetworkInfo.IsServer)
-            {
-                return;
-            }
+        using var writer = FusionWriter.Create(SpawnResponseData.GetSize(barcode));
+        var data = SpawnResponseData.Create(owner, barcode, syncId, serializedTransform);
+        writer.Write(data);
 
-            using var writer = FusionWriter.Create(SpawnResponseData.GetSize(barcode));
-            var data = SpawnResponseData.Create(owner, barcode, syncId, serializedTransform);
-            writer.Write(data);
-
-            using var message = FusionMessage.Create(NativeMessageTag.SpawnResponse, writer);
-            MessageSender.SendFromServer(userId, NetworkChannel.Reliable, message);
-        }
+        using var message = FusionMessage.Create(NativeMessageTag.SpawnResponse, writer);
+        MessageSender.SendFromServer(userId, NetworkChannel.Reliable, message);
     }
 }
