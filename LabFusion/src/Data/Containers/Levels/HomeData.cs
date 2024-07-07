37f1b18bea480dce5228eeb2a31c4d267dbdceb1
Logan Shockley <loganv182@gmail.com>
2024-07-06 23:19:26 -0500

    Removed automatically synced objects that error and dont work anymore

--- LabFusion/src/Data/Containers/Levels/HomeData.cs
+++ LabFusion/src/Data/Containers/Levels/HomeData.cs
@@ -2,113 +2,90 @@
 
 using Il2CppSLZ.Bonelab;
 using Il2CppSLZ.Vehicle;
+using Il2CppSLZ.Marrow.Interaction;
 
 using LabFusion.Utilities;
 using LabFusion.Extensions;
 using LabFusion.Network;
-using LabFusion.Senders;
 using LabFusion.Player;
 using LabFusion.SDK.Achievements;
-using Il2CppSLZ.Marrow.Interaction;
 
-namespace LabFusion.Data
+namespace LabFusion.Data;
+
+public class HomeData : LevelDataHandler
 {
-    public class HomeData : LevelDataHandler
+    public override string LevelTitle => "14 - Home";
+
+    public static GameControl_Outro GameController;
+    public static TaxiController TaxiController;
+    public static Seat TaxiSeat;
+    public static ArticulatedArmController ArmController;
+    public static ArmFinale ArmFinale;
+
+    protected override void MainSceneInitialized()
     {
-        public override string LevelTitle => "14 - Home";
-
-        public static GameControl_Outro GameController;
-        public static TaxiController TaxiController;
-        public static Seat TaxiSeat;
-        public static ArticulatedArmController ArmController;
-        public static ArmFinale ArmFinale;
-
-        protected override void MainSceneInitialized()
+        GameController = GameObject.FindObjectOfType<GameControl_Outro>(true);
+        if (GameController != null)
         {
-            GameController = GameObject.FindObjectOfType<GameControl_Outro>(true);
-            if (GameController != null)
+            // In a server, teleport the player to the top of the lift so they don't spawn underneath it if its synced
+            if (NetworkInfo.HasServer)
             {
-                // In a server, teleport the player to the top of the lift so they don't spawn underneath it if its synced
-                if (NetworkInfo.HasServer)
-                {
-                    FusionPlayer.Teleport(new Vector3(-9.030009f, -5.142975f, -71.18999f), Vector3Extensions.forward, true);
-                }
-
-                TaxiController = GameObject.FindObjectOfType<TaxiController>(true);
-                TaxiSeat = TaxiController.rearSeat;
-                var taxiEntity = TaxiController.GetComponentInParent<MarrowEntity>();
-
-                ArmController = GameObject.FindObjectOfType<ArticulatedArmController>(true);
-                ArmFinale = GameObject.FindObjectOfType<ArmFinale>(true);
-
-                // Hook seat event
-                TaxiSeat.RegisteredEvent += (Il2CppSystem.Action)OnTaxiSeatRegistered;
-
-                // Add extra seats
-                // Inside seat
-                Internal_CreateSeat(2, new Vector3(-0.326f, 0.441f, -1.125f), Vector3Extensions.zero);
-
-                // Trunk seats
-                Internal_CreateSeat(3, new Vector3(0.48f, 0.928f, -2.138f), Vector3Extensions.up * -180f);
-                Internal_CreateSeat(4, new Vector3(-0.48f, 0.928f, -2.138f), Vector3Extensions.up * -180f);
-
-                // Hood seats
-                Internal_CreateSeat(5, new Vector3(0.48f, 0.928f, 1.998f), Vector3Extensions.zero);
-                Internal_CreateSeat(6, new Vector3(-0.48f, 0.928f, 1.998f), Vector3Extensions.zero);
-
-                // Send the taxi, windmill, and lift to sync
-                if (NetworkInfo.IsServer)
-                {
-                    // Taxi
-                    PropSender.SendPropCreation(taxiEntity, null, true);
-
-                    // Windmill
-                    var windmill = ArmFinale.transform.Find("WindMill (2)");
-                    if (windmill != null)
-                    {
-                        PropSender.SendPropCreation(windmill.GetComponentInParent<MarrowEntity>());
-                    }
-                    else
-                    {
-#if DEBUG
-                        FusionLogger.Warn("Failed to find Windmill!");
-#endif
-                    }
-                }
+                FusionPlayer.Teleport(new Vector3(-9.030009f, -5.142975f, -71.18999f), Vector3Extensions.forward, true);
             }
+
+            TaxiController = GameObject.FindObjectOfType<TaxiController>(true);
+            TaxiSeat = TaxiController.rearSeat;
+
+            ArmController = GameObject.FindObjectOfType<ArticulatedArmController>(true);
+            ArmFinale = GameObject.FindObjectOfType<ArmFinale>(true);
+
+            // Hook seat event
+            TaxiSeat.RegisteredEvent += (Il2CppSystem.Action)OnTaxiSeatRegistered;
+
+            // Add extra seats
+            // Inside seat
+            CreateSeat(2, new Vector3(-0.326f, 0.441f, -1.125f), Vector3Extensions.zero);
+
+            // Trunk seats
+            CreateSeat(3, new Vector3(0.48f, 0.928f, -2.138f), Vector3Extensions.up * -180f);
+            CreateSeat(4, new Vector3(-0.48f, 0.928f, -2.138f), Vector3Extensions.up * -180f);
+
+            // Hood seats
+            CreateSeat(5, new Vector3(0.48f, 0.928f, 1.998f), Vector3Extensions.zero);
+            CreateSeat(6, new Vector3(-0.48f, 0.928f, 1.998f), Vector3Extensions.zero);
         }
+    }
 
-        private static void OnTaxiSeatRegistered()
+    private static void OnTaxiSeatRegistered()
+    {
+        // Give the achievement in a server with more than 1 player
+        if (NetworkInfo.HasServer && PlayerIdManager.HasOtherPlayers)
         {
-            // Give the achievement in a server with more than 1 player
-            if (NetworkInfo.HasServer && PlayerIdManager.HasOtherPlayers)
-            {
-                // Increment the achievement task
-                if (AchievementManager.TryGetAchievement<OneMoreTime>(out var achievement))
-                    achievement.IncrementTask();
-            }
+            // Increment the achievement task
+            if (AchievementManager.TryGetAchievement<OneMoreTime>(out var achievement))
+                achievement.IncrementTask();
         }
+    }
 
-        private static void Internal_CreateSeat(int index, Vector3 localPosition, Vector3 localRotation)
+    private static void CreateSeat(int index, Vector3 localPosition, Vector3 localRotation)
+    {
+        var extraSeat = GameObject.Instantiate(TaxiSeat.gameObject);
+        extraSeat.transform.parent = TaxiSeat.transform.parent;
+        extraSeat.SetActive(true);
+        extraSeat.name = $"Seat ({index})";
+
+        extraSeat.transform.SetLocalPositionAndRotation(localPosition, Quaternion.Euler(localRotation));
+    }
+
+    public static void TeleportToJimmyFinger()
+    {
+        if (RigData.HasPlayer)
         {
-            var extraSeat = GameObject.Instantiate(TaxiSeat.gameObject);
-            extraSeat.transform.parent = TaxiSeat.transform.parent;
-            extraSeat.SetActive(true);
-            extraSeat.name = $"Seat ({index})";
+            var rm = RigData.RigReferences.RigManager;
+            var pos = new Vector3(-0.25f, 95.23f, 13f);
 
-            extraSeat.transform.SetLocalPositionAndRotation(localPosition, Quaternion.Euler(localRotation));
-        }
-
-        public static void TeleportToJimmyFinger()
-        {
-            if (RigData.HasPlayer)
-            {
-                var rm = RigData.RigReferences.RigManager;
-                var pos = new Vector3(-0.25f, 95.23f, 13f);
-
-                rm.Teleport(pos, true);
-                rm.physicsRig.ResetHands(Handedness.BOTH);
-            }
+            rm.Teleport(pos, true);
+            rm.physicsRig.ResetHands(Handedness.BOTH);
         }
     }
 }
