43fcff764002ac9130c62824dfd4133a894985f9
Logan Shockley <loganv182@gmail.com>
2024-08-02 17:22:57 -0500

    Made serializable class for mod io files

---
+++ LabFusion/src/Downloading/ModIO/SerializableModIOFile.cs
@@ -0,0 +1,39 @@
+﻿using LabFusion.Data;
+using LabFusion.Network;
+
+namespace LabFusion.Downloading.ModIO;
+
+public class SerializableModIOFile : IFusionSerializable
+{
+    public ModIOFile File { get; private set; }
+
+    public bool HasFile { get; private set; }
+
+    public void Serialize(FusionWriter writer)
+    {
+        writer.Write(File.ModId);
+        writer.Write(HasFile);
+    }
+
+    public void Deserialize(FusionReader reader)
+    {
+        File = new ModIOFile(
+            reader.ReadInt32(),
+            null
+        );
+
+        HasFile = reader.ReadBoolean();
+    }
+
+    public SerializableModIOFile() { }
+
+    public SerializableModIOFile(ModIOFile? file)
+    {
+        HasFile = file.HasValue;
+
+        if (HasFile)
+        {
+            this.File = file.Value;
+        }
+    }
+}
