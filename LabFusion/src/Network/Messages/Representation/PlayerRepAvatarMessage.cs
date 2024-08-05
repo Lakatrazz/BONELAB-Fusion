91c341a7f599eaaeaec86a15e56ce24de6d782fb
DayTrip10 <136037341+DayTrip10@users.noreply.github.com>
2024-08-05 17:30:04 -0400

    Avatar Downloading working
    Full avatar downloading

--- LabFusion/src/Network/Messages/Representation/PlayerRepAvatarMessage.cs
+++ LabFusion/src/Network/Messages/Representation/PlayerRepAvatarMessage.cs
@@ -1,62 +1,123 @@
 ﻿using LabFusion.Data;
 using LabFusion.Entities;
 using LabFusion.Player;
+using LabFusion.Utilities;
+using LabFusion.Downloading.ModIO;
+using Il2CppSLZ.Marrow.Pool;
+using Il2CppSLZ.Marrow.Warehouse;
+using Il2CppSLZ.Marrow.Data;
+using Il2CppSLZ.Marrow.Interaction;
+using LabFusion.Extensions;
+using LabFusion.Exceptions;
+using LabFusion.Senders;
+using LabFusion.RPC;
+using LabFusion.Marrow;
+using LabFusion.Downloading;
 
-namespace LabFusion.Network;
-
-public class PlayerRepAvatarData : IFusionSerializable
+namespace LabFusion.Network
 {
-    public const int DefaultSize = sizeof(byte) + SerializedAvatarStats.Size;
-
-    public byte smallId;
-    public SerializedAvatarStats stats;
-    public string barcode;
-
-    public void Serialize(FusionWriter writer)
+    public class PlayerRepAvatarData : IFusionSerializable
     {
-        writer.Write(smallId);
-        writer.Write(stats);
-        writer.Write(barcode);
-    }
+        public const int DefaultSize = sizeof(byte) + SerializedAvatarStats.Size;
 
-    public void Deserialize(FusionReader reader)
-    {
-        smallId = reader.ReadByte();
-        stats = reader.ReadFusionSerializable<SerializedAvatarStats>();
-        barcode = reader.ReadString();
-    }
+        public byte smallId;
+        public SerializedAvatarStats stats;
+        public string barcode;
 
-    public static PlayerRepAvatarData Create(byte smallId, SerializedAvatarStats stats, string barcode)
-    {
-        return new PlayerRepAvatarData()
+        public void Serialize(FusionWriter writer)
         {
-            smallId = smallId,
-            stats = stats,
-            barcode = barcode
-        };
-    }
-}
-
-public class PlayerRepAvatarMessage : FusionMessageHandler
-{
-    public override byte Tag => NativeMessageTag.PlayerRepAvatar;
-
-    public override void HandleMessage(byte[] bytes, bool isServerHandled = false)
-    {
-        using var reader = FusionReader.Create(bytes);
-        var data = reader.ReadFusionSerializable<PlayerRepAvatarData>();
-
-        // Swap the avatar for the rep
-        if (NetworkPlayerManager.TryGetPlayer(data.smallId, out var player))
-        {
-            player.AvatarSetter.SwapAvatar(data.stats, data.barcode);
+            writer.Write(smallId);
+            writer.Write(stats);
+            writer.Write(barcode);
         }
 
-        // Bounce the message back
-        if (NetworkInfo.IsServer)
+        public void Deserialize(FusionReader reader)
         {
-            using var message = FusionMessage.Create(Tag, bytes);
-            MessageSender.BroadcastMessageExcept(data.smallId, NetworkChannel.Reliable, message);
+            smallId = reader.ReadByte();
+            stats = reader.ReadFusionSerializable<SerializedAvatarStats>();
+            barcode = reader.ReadString();
+        }
+
+        public static PlayerRepAvatarData Create(byte smallId, SerializedAvatarStats stats, string barcode)
+        {
+            return new PlayerRepAvatarData()
+            {
+                smallId = smallId,
+                stats = stats,
+                barcode = barcode
+            };
+        }
+    }
+
+    [Net.DelayWhileTargetLoading]
+    public class PlayerRepAvatarMessage : FusionMessageHandler
+    {
+        public override byte Tag => NativeMessageTag.PlayerRepAvatar;
+
+        public override void HandleMessage(byte[] bytes, bool isServerHandled = false)
+        {
+            using var reader = FusionReader.Create(bytes);
+            var data = reader.ReadFusionSerializable<PlayerRepAvatarData>();
+
+            if (NetworkPlayerManager.TryGetPlayer(data.smallId, out var player))
+            {
+                if (!AvatarExists(data.barcode))
+                {
+                    RequestAndDownloadAvatar(data, player);
+                }
+                else
+                {
+                    player.AvatarSetter.SwapAvatar(data.stats, data.barcode);
+                }
+            }
+
+            if (NetworkInfo.IsServer)
+            {
+                using var message = FusionMessage.Create(Tag, bytes);
+                MessageSender.BroadcastMessageExcept(data.smallId, NetworkChannel.Reliable, message);
+            }
+        }
+
+        private bool AvatarExists(string barcode)
+        {
+            // Implement logic to check if the avatar with the given barcode exists locally
+            return false;
+        }
+
+        private void RequestAndDownloadAvatar(PlayerRepAvatarData data, NetworkPlayer player)
+        {
+            NetworkModRequester.RequestMod(new NetworkModRequester.ModRequestInfo()
+            {
+                target = data.smallId,
+                barcode = data.barcode,
+                modCallback = OnModInfoReceived,
+            });
+
+            void OnModInfoReceived(NetworkModRequester.ModCallbackInfo info)
+            {
+                if (!info.hasFile)
+                {
+                    return;
+                }
+
+                ModIODownloader.EnqueueDownload(new ModTransaction()
+                {
+                    modFile = info.modFile,
+                    temporary = true,
+                    callback = OnModDownloaded,
+                });
+            }
+
+            void OnModDownloaded(DownloadCallbackInfo info)
+            {
+                if (info.result == ModResult.FAILED)
+                {
+                    FusionLogger.Warn($"Failed downloading avatar {data.barcode}!");
+                    return;
+                }
+
+                player.AvatarSetter.SwapAvatar(data.stats, data.barcode);
+            }
         }
     }
 }
