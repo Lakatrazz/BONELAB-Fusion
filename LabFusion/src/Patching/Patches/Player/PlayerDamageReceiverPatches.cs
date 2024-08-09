6ead6870cdb6c94581217d2648d6c55c131fdbe1
Logan Shockley <loganv182@gmail.com>
2024-08-09 13:56:25 -0500

    Made all damage types sync from player combat

--- LabFusion/src/Patching/Patches/Player/PlayerDamageReceiverPatches.cs
+++ LabFusion/src/Patching/Patches/Player/PlayerDamageReceiverPatches.cs
@@ -3,7 +3,6 @@ using LabFusion.Utilities;
 using LabFusion.Senders;
 using LabFusion.Entities;
 
-using Il2CppSLZ.Marrow.Data;
 using Il2CppSLZ.Marrow;
 using Il2CppSLZ.Marrow.AI;
 using Il2CppSLZ.Marrow.Combat;
@@ -15,11 +14,6 @@ namespace LabFusion.Patching;
 [HarmonyPatch(typeof(PlayerDamageReceiver))]
 public static class PlayerDamageReceiverPatches
 {
-    public static bool IsSynced(AttackType type)
-    {
-        return type == AttackType.Blunt || type == AttackType.Piercing;
-    }
-
     [HarmonyPatch(nameof(PlayerDamageReceiver.ReceiveAttack))]
     [HarmonyPrefix]
     public static bool ReceiveAttack(PlayerDamageReceiver __instance, ref Attack attack)
@@ -31,33 +25,30 @@ public static class PlayerDamageReceiverPatches
 
         var rm = __instance.health._rigManager;
 
-        // Get the attack and its shooter
+        // Get the attack and its attacker
         TriggerRefProxy proxy = attack.proxy;
 
-        RigManager shooter = null;
+        RigManager attacker = null;
 
         if (proxy != null && proxy.root != null)
         {
-            shooter = RigManager.Cache.Get(proxy.root);
+            attacker = RigManager.Cache.Get(proxy.root);
         }
 
         // Make sure we have the attacker and attacked
-        if (rm != null && shooter != null)
+        if (rm != null && attacker != null)
         {
             // Is the attacked person us?
             if (rm.IsSelf())
             {
                 // Were we hit by another player?
-                if (NetworkPlayerManager.TryGetPlayer(shooter, out var player) && !player.NetworkEntity.IsOwner)
+                if (NetworkPlayerManager.TryGetPlayer(attacker, out var player) && !player.NetworkEntity.IsOwner)
                 {
                     FusionPlayer.LastAttacker = player.PlayerId;
 
-                    // Only allow manual damage for certain types
-                    if (IsSynced(attack.attackType))
-                    {
-                        attack.damage = 0f;
-                        return true;
-                    }
+                    // Only allow manual damage
+                    attack.damage = 0f;
+                    return true;
                 }
                 // Were we hit by ourselves?
                 else
@@ -66,7 +57,7 @@ public static class PlayerDamageReceiverPatches
                 }
             }
             // Is the attacked person another player? Did we attack them?
-            else if (IsSynced(attack.attackType) && NetworkPlayerManager.TryGetPlayer(rm, out var player) && shooter.IsSelf())
+            else if (NetworkPlayerManager.TryGetPlayer(rm, out var player) && attacker.IsSelf())
             {
                 // Send the damage over the network
                 PlayerSender.SendPlayerDamage(player.PlayerId, attack, __instance.bodyPart);
