789ff1336395f399d8056353ef1123deac3ddf6c
Logan Shockley <loganv182@gmail.com>
2024-08-11 20:04:21 -0500

    Made asset spawner use proper null scale and groupId

--- LabFusion/src/Marrow/AssetWarehouse/SafeAssetSpawner.cs
+++ LabFusion/src/Marrow/AssetWarehouse/SafeAssetSpawner.cs
@@ -12,7 +12,17 @@ public static class SafeAssetSpawner
         // spawnCallback and despawnCallback no longer seem to work in patch 4 through MelonLoader
         // Instead, we use the async variant and simply continue when the method finishes
         // This originally spawned directly from the pool, however that seemed to have strange effects on spawned objects
-        var spawnTask = AssetSpawner.SpawnAsync(spawnable, position, rotation, new(Vector3.one), null, false, new(0), null, null);
+        var scale = new Il2CppSystem.Nullable<Vector3>(Vector3.zero)
+        {
+            hasValue = false,
+        };
+
+        var groupId = new Il2CppSystem.Nullable<int>(0)
+        {
+            hasValue = false,
+        };
+
+        var spawnTask = AssetSpawner.SpawnAsync(spawnable, position, rotation, scale, null, false, groupId, null, null);
         var awaiter = spawnTask.GetAwaiter();
 
         var continuation = () =>
