3283dd6d485126105d0757bf092bc8d4a0bedca9
Logan Shockley <loganv182@gmail.com>
2024-08-28 17:29:03 -0500

    Fixed only one gokart spawning in monogon motorway

--- LabFusion/src/Data/Containers/Levels/KartRaceData.cs
+++ LabFusion/src/Data/Containers/Levels/KartRaceData.cs
@@ -3,84 +3,94 @@
 using LabFusion.Network;
 using LabFusion.Player;
 using LabFusion.Utilities;
+using LabFusion.Scene;
 
-using Il2CppSLZ.Interaction;
 using Il2CppSLZ.Marrow.Warehouse;
 using Il2CppSLZ.Bonelab;
+using Il2CppSLZ.Marrow.VoidLogic;
 
-using UnityEngine.Events;
+namespace LabFusion.Data;
 
-namespace LabFusion.Data
+public class KartRaceData : LevelDataHandler
 {
-    public class KartRaceData : LevelDataHandler
+    public override string LevelTitle => "10 - Monogon Motorway";
+
+    public const string GokartName = "CrateSpawner (Gokart)";
+
+    public const string ResetButtonName = "button_3x_Toggle_Powered_RESETRACE";
+
+    private static readonly Vector3[] ExtraKartPositions = new Vector3[5]
     {
-        public override string LevelTitle => "10 - Monogon Motorway";
+        new(-3.62f, -5.222564f, 6.46f),
+        new(-1.61f, -5.222564f, 3.77f),
+        new(0.89f, -5.222564f, 6.29f),
+        new(3.01f, -5.222564f, 3.62f),
+        new(5.85f, -5.222564f, 5.91f),
+    };
 
-        private static readonly Vector3[] ExtraKartPositions = new Vector3[5]
+    public static GameControl_KartRace GameController;
+
+    protected override void MainSceneInitialized()
+    {
+        GameController = GameObject.FindObjectOfType<GameControl_KartRace>();
+        SpawnExtraKarts();
+    }
+
+    private static void SpawnExtraKarts()
+    {
+        // Make sure we have a server
+        if (!NetworkInfo.HasServer)
         {
-            new Vector3(-3.62f, -5.222564f, 6.46f),
-            new Vector3(-1.61f, -5.222564f, 3.77f),
-            new Vector3(0.89f, -5.222564f, 6.29f),
-            new Vector3(3.01f, -5.222564f, 3.62f),
-            new Vector3(5.85f, -5.222564f, 5.91f),
-        };
-
-        public static GameControl_KartRace GameController;
-
-        protected override void MainSceneInitialized()
-        {
-            GameController = GameObject.FindObjectOfType<GameControl_KartRace>();
-            SpawnExtraKarts();
+            return;
         }
 
-        private static void SpawnExtraKarts()
+        // If we are in Monogon Motorway, spawn extra go-karts
+        // Link these spawns up to the reset race button too
+        var extraPlayers = PlayerIdManager.PlayerCount - 1;
+
+        // Get the gokart spawner
+        GameObject gokartSpawner = GameObject.Find(GokartName);
+
+        // Get the reset button
+        var resetButtonGo = GameObject.Find(ResetButtonName);
+
+        EventAdapter resetEventAdapter = null;
+
+        if (resetButtonGo != null)
         {
-            // If we are in Monogon Motorway, spawn extra go-karts
-            // Link these spawns up to the reset race button too
-            if (GameController != null && NetworkInfo.HasServer)
+            resetEventAdapter = resetButtonGo.GetComponentInChildren<EventAdapter>();
+        }
+
+        // Now actually create each go kart
+        if (gokartSpawner)
+        {
+            for (var i = 0; i < extraPlayers && i < ExtraKartPositions.Length; i++)
             {
-                var extraPlayers = PlayerIdManager.PlayerCount - 1;
+                // Create placers
+                var newPlacer = GameObject.Instantiate(gokartSpawner);
+                newPlacer.transform.parent = gokartSpawner.transform.parent;
+                newPlacer.transform.localPosition = ExtraKartPositions[i];
+                newPlacer.name = $"{gokartSpawner.name} Player {i + 1}";
+                var newPlacerScript = newPlacer.GetComponent<CrateSpawner>();
 
-                // Get the go kart placer
-                GameObject gokartPlacer = GameObject.Find("Spawnable Placer (Gokart)");
+                // Spawn the spawnable from the new placer
+                FusionSceneManager.HookOnDelayedLevelLoad(newPlacerScript.SpawnSpawnable);
 
-                // Get the reset button
-                var resetButtonGo = GameObject.Find("prop_bigButton_RESETRACE");
-
-                ButtonToggle resetButton = null;
-                if (resetButtonGo != null)
+                // Add to the reset button event
+                if (resetEventAdapter != null)
                 {
-                    resetButton = resetButtonGo.GetComponent<ButtonToggle>();
-                }
-
-                // Now actually create each go kart
-                if (gokartPlacer)
-                {
-                    for (var i = 0; i < extraPlayers && i < ExtraKartPositions.Length; i++)
+                    var inputRoseEvent = (EventAdapter adapter, IVoidLogicSource source, float value) =>
                     {
-                        // Create placers
-                        var newPlacer = GameObject.Instantiate(gokartPlacer);
-                        newPlacer.transform.parent = gokartPlacer.transform.parent;
-                        newPlacer.transform.localPosition = ExtraKartPositions[i];
-                        newPlacer.name = $"{gokartPlacer.name} Player {i + 1}";
-                        var newPlacerScript = newPlacer.GetComponent<CrateSpawner>();
+                        newPlacerScript.SpawnSpawnable();
+                    };
 
-                        newPlacerScript.ReSpawnSpawnable();
-
-                        // Add to reset button
-                        if (resetButton != null)
-                        {
-                            resetButton.onDepress.AddListener((UnityAction)newPlacerScript.ReSpawnSpawnable);
-                        }
-                    }
-                }
-                else
-                {
-#if DEBUG
-                    FusionLogger.Warn("Monogon Motorway is missing the go kart placer!");
-#endif
+                    resetEventAdapter.InputRose.add_DynamicCalls(inputRoseEvent);
                 }
             }
+        }
+        else
+        {
+            FusionLogger.Warn("Could not find the Gokart spawner in Monogon Motorway, please report this to Lakatrazz!");
         }
     }
 }
