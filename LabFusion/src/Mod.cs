6c2d4ad06aecec5d2e32583ba28884a76fe39689
Logan Shockley <loganv182@gmail.com>
2024-11-30 11:23:02 -0600

    Updated mod version

--- LabFusion/src/Mod.cs
+++ LabFusion/src/Mod.cs
@@ -45,9 +45,9 @@ public struct FusionVersion
 #else
     public const byte VersionMajor = 1;
     public const byte VersionMinor = 9;
-    public const short VersionPatch = 2;
+    public const short VersionPatch = 3;
 
-    public const string VersionString = "1.9.2";
+    public const string VersionString = "1.9.3";
 #endif
 }
 
