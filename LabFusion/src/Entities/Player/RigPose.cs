597bf6ee8299fd3862e070ea990aa7a377d59b89
Logan Shockley <loganv182@gmail.com>
2025-01-18 19:59:51 -0600

    Undid change

--- LabFusion/src/Entities/Player/RigPose.cs
+++ LabFusion/src/Entities/Player/RigPose.cs
@@ -28,7 +28,7 @@ public class RigPose : IFusionSerializable
         }
 
         // Read playspace
-        trackedPlayspace = SerializedSmallQuaternion.Compress(skeleton.trackedPlayspace.localRotation);
+        trackedPlayspace = SerializedSmallQuaternion.Compress(skeleton.trackedPlayspace.rotation);
 
         // Read bodies
         pelvisPose.ReadFrom(skeleton.physicsPelvis);
