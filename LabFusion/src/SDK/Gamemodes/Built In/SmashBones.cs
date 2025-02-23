20ceca3a093c1c33320fe75d2ca0147a5c84dad0
Logan Shockley <loganv182@gmail.com>
2025-02-23 01:36:29 -0600

    Again

--- LabFusion/src/SDK/Gamemodes/Built In/SmashBones.cs
+++ LabFusion/src/SDK/Gamemodes/Built In/SmashBones.cs
@@ -266,7 +266,7 @@ public class SmashBones : Gamemode
             magnitude *= Mathf.Sqrt(avatarMass);
 
             var force = direction * magnitude;
-            var upForce = -Physics.gravity.normalized * magnitude * 0.6f;
+            var upForce = -Physics.gravity.normalized * magnitude * 0.3f;
 
             pelvisRb.AddForce(force + upForce, ForceMode.Impulse);
 
