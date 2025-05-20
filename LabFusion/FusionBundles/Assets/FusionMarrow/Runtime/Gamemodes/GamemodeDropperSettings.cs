55d71f0b2522ee01bd297cbd06ccded5e07fa7fc
Logan Shockley <loganv182@gmail.com>
2025-05-20 17:04:58 -0500

    Added a bunch more props

--- LabFusion/FusionBundles/Assets/FusionMarrow/Runtime/Gamemodes/GamemodeDropperSettings.cs
+++ LabFusion/FusionBundles/Assets/FusionMarrow/Runtime/Gamemodes/GamemodeDropperSettings.cs
@@ -45,19 +45,30 @@ namespace LabFusion.Marrow.Integration
             new ItemDrop() { ItemCrateReference = new("c1534c5a-1f5a-4993-bbc1-03be4d656c65"), Probability = 19f, }, // Sledgehammer
             new ItemDrop() { ItemCrateReference = new("c1534c5a-6d15-47c7-9ad4-b04156696b69"), Probability = 50f, }, // Viking Shield
             new ItemDrop() { ItemCrateReference = new("c1534c5a-282b-4430-b009-58954b617461"), Probability = 5f, }, // Katana
-            new ItemDrop() { ItemCrateReference = new("c1534c5a-f6f9-4c96-b88e-91d74c656164"), Probability = 53f, }, // Lead Pipe
+            new ItemDrop() { ItemCrateReference = new("c1534c5a-f6f9-4c96-b88e-91d74c656164"), Probability = 55f, }, // Lead Pipe
             new ItemDrop() { ItemCrateReference = new("c1534c5a-4774-460f-a814-149541786546"), Probability = 30f, }, // Axe Firefighter
             new ItemDrop() { ItemCrateReference = new("SLZ.BONELAB.Content.Spawnable.ElectricGuitar"), Probability = 41f, }, // Electric Guitar
+            new ItemDrop() { ItemCrateReference = new("c1534c5a-02e7-43cf-bc8d-26955772656e"), Probability = 45f, }, // Wrench
+            new ItemDrop() { ItemCrateReference = new("c1534c5a-5d31-488d-b5b3-aa1c53686f76"), Probability = 30f, }, // Shovel
+            new ItemDrop() { ItemCrateReference = new("c1534c5a-d0e9-4d53-9218-e76446727969"), Probability = 50f, }, // Frying Pan
+            new ItemDrop() { ItemCrateReference = new("c1534c5a-0c8a-4b82-9f8b-7a9543726f77"), Probability = 55f, }, // Crowbar
+            new ItemDrop() { ItemCrateReference = new("c1534c5a-1fb8-477c-afbe-2a95436f6d62"), Probability = 35f, }, // Combat Knife
+            new ItemDrop() { ItemCrateReference = new("c1534c5a-f0d1-40b6-9f9b-c19544616767"), Probability = 25f, }, // Kunai
+            new ItemDrop() {ItemCrateReference = new("c1534c5a-a97f-4bff-b512-e44d53706561"), Probability = 20f, }, // Spear
 
             // Guns
             new ItemDrop() { ItemCrateReference = new("c1534c5a-ec8e-418a-a545-cf955269666c"), Probability = 11f, }, // MK18 Laser Foregrip
             new ItemDrop() { ItemCrateReference = new("SLZ.BONELAB.CORE.Spawnable.GunEHG"), Probability = 15f, }, // e-HG Blaster
             new ItemDrop() { ItemCrateReference = new("c1534c5a-2a4f-481f-8542-cc9545646572"), Probability = 22f, }, // Eder22
+            new ItemDrop() { ItemCrateReference = new("c1534c5a-a6b5-4177-beb8-04d947756e41"), Probability = 5f, }, // AKM
+            new ItemDrop() {ItemCrateReference = new("SLZ.BONELAB.Content.Spawnable.RifleM1Garand"), Probability = 18f, }, // M1 Garand
 
             // Gadgets
             new ItemDrop() { ItemCrateReference = new("c1534c5a-e963-4a7c-8c7e-1195546f7942"), Probability = 50f, }, // Toy Balloon Gun
             new ItemDrop() { ItemCrateReference = new("c1534c5a-c6a8-45d0-aaa2-2c954465764d"), Probability = 5f, }, // Dev Manipulator
-            new ItemDrop() { ItemCrateReference = new("c1534c5a-e777-4d15-b0c1-3195426f6172"), Probability = 10f, }, // Boardgun
+            new ItemDrop() { ItemCrateReference = new("c1534c5a-e777-4d15-b0c1-3195426f6172"), Probability = 20f, }, // Boardgun
+            new ItemDrop() { ItemCrateReference = new("c1534c5a-cebf-42cc-be3a-4595506f7765"), Probability = 10f }, // Power Puncher
+            new ItemDrop() { ItemCrateReference = new("c1534c5a-87ce-436d-b00c-ef9547726176"), Probability = 10f, }, // Gravity Cup
             // new ItemDrop() { ItemCrateReference = new("SLZ.BONELAB.Content.Spawnable.PropHealthPickup"), Probability = 20f, }, // Prop_Health Pickup, add functionality later
 
             // Props
@@ -75,6 +86,10 @@ namespace LabFusion.Marrow.Integration
             new ItemDrop() { ItemCrateReference = new("SLZ.BONELAB.Content.Spawnable.Died20"), Probability = 70f, }, // Die D20
             new ItemDrop() { ItemCrateReference = new("SLZ.BONELAB.Content.Spawnable.ClayPotA"), Probability = 69f, }, // Clay Pot A
             new ItemDrop() { ItemCrateReference = new("SLZ.BONELAB.Content.Spawnable.ClayVase"), Probability = 51f, }, // Clay Vase
+            new ItemDrop() { ItemCrateReference = new("c1534c5a-9629-4660-8439-186b50726f70"), Probability = 70f, }, // Coffee Cup
+            new ItemDrop() { ItemCrateReference = new("SLZ.BONELAB.Content.Spawnable.BowlingPin"), Probability = 70f, }, // Bowling Pin
+            new ItemDrop() { ItemCrateReference = new("c1534c5a-3199-4102-91e2-8ac650726f70"), Probability = 45f, }, // Gym D20
+            new ItemDrop() {ItemCrateReference = new("c1534c5a-202f-43f8-9a6c-1e9450726f70"), Probability = 5f, }, // Monkey
 
             // NPCs
             new ItemDrop() { ItemCrateReference = new("c1534c5a-ef15-44c0-88ae-aebc4e756c6c"), Probability = 11f, }, // Null Rat
