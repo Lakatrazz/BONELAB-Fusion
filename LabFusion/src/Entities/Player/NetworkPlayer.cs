8f564fbc3d6bdcf655aa340122d11b6a9eb7f67d
Logan Shockley <loganv182@gmail.com>
2025-06-11 22:39:02 -0500

    Error catch

--- LabFusion/src/Entities/Player/NetworkPlayer.cs
+++ LabFusion/src/Entities/Player/NetworkPlayer.cs
@@ -821,7 +821,7 @@ public class NetworkPlayer : IEntityExtender, IMarrowEntityExtender, IEntityUpda
         _pose = null;
         _receivedPose = false;
 
-        NetworkEntity.ClearDataCaughtUpPlayers();
+        NetworkEntity?.ClearDataCaughtUpPlayers();
 
         UnregisterComponents();
     }
