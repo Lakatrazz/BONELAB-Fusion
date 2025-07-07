abc504a4a289ef0f5fe49a234ce05753dab43dcc
Checkerboard <Colehennessy1@gmail.com>
2025-07-06 23:02:38 -0700

    file formatting

--- LabFusion/src/Network/Layers/EOS/EOSManager.cs
+++ LabFusion/src/Network/Layers/EOS/EOSManager.cs
@@ -142,19 +142,19 @@ internal class EOSManager
 			return false;
 		}
 
-        // Setup debug logging
-        Epic.OnlineServices.Logging.LoggingInterface.SetLogLevel(Epic.OnlineServices.Logging.LogCategory.AllCategories, EOSNetworkLayer.LogLevel);
-        Epic.OnlineServices.Logging.LoggingInterface.SetCallback((ref Epic.OnlineServices.Logging.LogMessage logMessage) =>
-        {
-            // https://eoshelp.epicgames.com/s/article/Why-is-the-warning-LogEOS-FEpicGamesPlatform-GetOnlinePlatformType-unable-to-map-None-to-EOS-OnlinePlatformType-thrown?language=en_US
-            if (logMessage.Message == "FEpicGamesPlatform::GetOnlinePlatformType - unable to map None to EOS_OnlinePlatformType")
-                return;
+		// Setup debug logging
+		Epic.OnlineServices.Logging.LoggingInterface.SetLogLevel(Epic.OnlineServices.Logging.LogCategory.AllCategories, EOSNetworkLayer.LogLevel);
+		Epic.OnlineServices.Logging.LoggingInterface.SetCallback((ref Epic.OnlineServices.Logging.LogMessage logMessage) =>
+		{
+			// https://eoshelp.epicgames.com/s/article/Why-is-the-warning-LogEOS-FEpicGamesPlatform-GetOnlinePlatformType-unable-to-map-None-to-EOS-OnlinePlatformType-thrown?language=en_US
+			if (logMessage.Message == "FEpicGamesPlatform::GetOnlinePlatformType - unable to map None to EOS_OnlinePlatformType")
+				return;
 
-            FusionLogger.Log(logMessage.Message);
-        });
+			FusionLogger.Log(logMessage.Message);
+		});
 
 
-        AuthInterface = PlatformInterface.GetAuthInterface();
+		AuthInterface = PlatformInterface.GetAuthInterface();
 		ConnectInterface = PlatformInterface.GetConnectInterface();
 		P2PInterface = PlatformInterface.GetP2PInterface();
 		LobbyInterface = PlatformInterface.GetLobbyInterface();
