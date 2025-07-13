7406041adc8ae4ba2881b05f82484b94d96fc790
Checkerboard <Colehennessy1@gmail.com>
2025-07-13 03:33:27 -0700

    Clean up EOS Utils

--- LabFusion/src/Network/Layers/EOS/EOSUtils.cs
+++ LabFusion/src/Network/Layers/EOS/EOSUtils.cs
@@ -4,8 +4,6 @@ using Epic.OnlineServices.UserInfo;
 
 using LabFusion.Utilities;
 
-using System.Collections;
-
 namespace LabFusion.Network;
 
 internal class EOSUtils
@@ -22,6 +20,7 @@ internal class EOSUtils
         var queryOptions = new QueryProductUserIdMappingsOptions
         {
             LocalUserId = EOSNetworkLayer.LocalUserId,
+            AccountIdType_DEPRECATED = ExternalAccountType.Epic,
             ProductUserIds = productUserIds
         };
         EOSManager.ConnectInterface.QueryProductUserIdMappings(ref queryOptions, null, null);
@@ -35,6 +34,34 @@ internal class EOSUtils
         EOSManager.ConnectInterface.GetProductUserIdMapping(ref mappingsOptions, out var epicAccountId);
 
         return EpicAccountId.FromString(epicAccountId);
+    }
+
+    internal static ProductUserId GetProductIdFromAccountId(EpicAccountId accountId)
+    {
+        if (accountId == null)
+            return null;
+
+        var epicAccountIds = new Utf8String[]
+        {
+            accountId.ToString()
+        };
+        var queryOptions = new QueryExternalAccountMappingsOptions
+        {
+            LocalUserId = EOSNetworkLayer.LocalUserId,
+            AccountIdType = ExternalAccountType.Epic,
+            ExternalAccountIds = epicAccountIds
+        };
+        EOSManager.ConnectInterface.QueryExternalAccountMappings(ref queryOptions, null, null);
+
+        var mappingsOptions = new GetExternalAccountMappingsOptions
+        {
+            LocalUserId = EOSNetworkLayer.LocalUserId,
+            AccountIdType = ExternalAccountType.Epic,
+            TargetExternalUserId = accountId.ToString()
+        };
+        ProductUserId productUserId = EOSManager.ConnectInterface.GetExternalAccountMapping(ref mappingsOptions);
+
+        return productUserId;
     }
 
     internal static string GetDisplayNameFromProductId(ProductUserId productUserId)
@@ -54,44 +81,14 @@ internal class EOSUtils
         }
         else if (result != Result.Success)
         {
-            FusionLogger.Warn($"Failed to get display name for ProductUserId {productUserId}: {result}");
             return null;
         }
 
         return null;
     }
 
-    internal static IEnumerator GetDisplayNameFromAccountId(EpicAccountId accountId, Action<string> onComplete)
+    internal static string GetDisplayNameFromAccountId(EpicAccountId accountId)
     {
-        var userInfoOptions = new QueryUserInfoOptions
-        {
-            LocalUserId = EOSNetworkLayer.LocalAccountId,
-            TargetUserId = accountId
-        };
-
-        TaskCompletionSource<string> usernameTask = new TaskCompletionSource<string>();
-        EOSManager.UserInfoInterface.QueryUserInfo(ref userInfoOptions, null, (ref QueryUserInfoCallbackInfo callbackInfo) =>
-        {
-            if (callbackInfo.ResultCode != Result.Success)
-            {
-                usernameTask.SetResult(string.Empty);
-                return;
-            }
-
-            var copyOptions = new CopyUserInfoOptions
-            {
-                LocalUserId = EOSNetworkLayer.LocalAccountId,
-                TargetUserId = accountId
-            };
-
-            if (EOSManager.UserInfoInterface.CopyUserInfo(ref copyOptions, out var userInfo) == Result.Success)
-                usernameTask.SetResult(userInfo.Value.DisplayName ?? "Unknown");
-        });
-
-        while (!usernameTask.Task.IsCompleted)
-            yield return null;
-
-        onComplete?.Invoke(usernameTask.Task.Result);
-        yield break;
+        return GetDisplayNameFromProductId(GetProductIdFromAccountId(accountId));
     }
 }
