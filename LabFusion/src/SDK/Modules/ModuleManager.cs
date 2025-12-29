8c574b44602c10d2dee9d0859777280cca2d1c1a
Logan Shockley <loganv182@gmail.com>
2025-12-29 15:50:54 -0600

    Added load modules from assembly method

--- LabFusion/src/SDK/Modules/ModuleManager.cs
+++ LabFusion/src/SDK/Modules/ModuleManager.cs
@@ -1,5 +1,7 @@
 ﻿using LabFusion.Utilities;
 
+using System.Reflection;
+
 namespace LabFusion.SDK.Modules;
 
 public static class ModuleManager
@@ -9,18 +11,39 @@ public static class ModuleManager
     public static List<Module> Modules => _modules;
 
     /// <summary>
+    /// Loads all <see cref="Module"/>s from an assembly.
+    /// </summary>
+    /// <param name="assembly">The assembly to load modules from.</param>
+    /// <exception cref="ArgumentNullException"></exception>
+    public static void LoadModules(Assembly assembly)
+    {
+        if (assembly == null)
+        {
+            throw new ArgumentNullException(nameof(assembly));
+        }
+
+        AssemblyUtilities.LoadAllValid<Module>(assembly, RegisterModule);
+    }
+
+    /// <summary>
     /// Registers a module of the given type.
     /// </summary>
-    /// <typeparam name="TModule">The type of the module.</typeparam>
-    public static void RegisterModule<TModule>() where TModule : Module
+    /// <param name="type">The type of the module.</param>
+    public static void RegisterModule(Type type)
     {
-        var module = Activator.CreateInstance<TModule>();
+        var module = (Module)Activator.CreateInstance(type);
 
         LogDescription(module);
 
         _modules.Add(module);
         module.Register();
     }
+
+    /// <summary>
+    /// Registers a module of the given type.
+    /// </summary>
+    /// <typeparam name="TModule">The type of the module.</typeparam>
+    public static void RegisterModule<TModule>() where TModule : Module => RegisterModule(typeof(TModule));
 
     private static void LogDescription(Module module)
     {
