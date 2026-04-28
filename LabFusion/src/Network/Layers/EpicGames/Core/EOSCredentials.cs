d9602319cdca670fed3a2d8a1cdf9cd546a4b592
Checkerboard <Colehennessy1@gmail.com>
2026-04-28 12:28:11 -0700

    Update EOSCredentials.cs

--- LabFusion/src/Network/Layers/EpicGames/Core/EOSCredentials.cs
+++ LabFusion/src/Network/Layers/EpicGames/Core/EOSCredentials.cs
@@ -5,8 +5,8 @@ internal static class EOSCredentials
     internal const string ProductName = "Fusion";
     internal const string ProductVersion = "0.0.1";
     internal const string ProductId = "29e074d5b4724f3bb01f26b7e33d2582";
-    internal const string ClientId = "xyza7891gWLwVJx3rdLOLs6vJ05u9jWT";
-    internal const string ClientSecret = "IWrUy1Z62wWajAX37k3zkQ4Kkto+AvfQSyZ9zfvibzw";
+    internal const string ClientId = "xyza78915hKqxe2TNTavpq2sxBDvJ9AH";
+    internal const string ClientSecret = "wBPaPmSI7dWUt87+nvs2pp7TeQVFXSDz+/PnSdYDyc0";
     internal const string SandboxId = "26f32d66d87f4dfeb4a7449b776a41f1";
-    internal const string DeploymentId = "1dffb21201e04ad89b0e6e415f0b8993";
+    internal const string DeploymentId = "76d456523b2d468dbde74e7ea6ddcd6b";
 }
