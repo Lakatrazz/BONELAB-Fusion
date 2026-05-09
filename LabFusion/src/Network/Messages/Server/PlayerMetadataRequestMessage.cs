e23b964ec4fadc52004f6f448208261161152a21
Logan Shockley <loganv182@gmail.com>
2026-05-09 10:53:32 -0500

    Locked setting networked PermissionLevel to host only

--- LabFusion/src/Network/Messages/Server/PlayerMetadataRequestMessage.cs
+++ LabFusion/src/Network/Messages/Server/PlayerMetadataRequestMessage.cs
@@ -1,4 +1,5 @@
-﻿using LabFusion.Senders;
+﻿using LabFusion.Player;
+using LabFusion.Senders;
 using LabFusion.Utilities;
 
 namespace LabFusion.Network;
@@ -13,15 +14,29 @@ public class PlayerMetadataRequestMessage : NativeMessageHandler
     {
         var data = received.ReadData<PlayerMetadataData>();
 
+        var key = data.Key;
+        var value = data.Value;
+        var playerID = data.Player.ID;
+
+        var sender = received.Sender;
+
         // Make sure the message sender is able to modify this player's metadata
-        if (!NetworkVerification.HasAuthorityOverPlayer(data.Player.ID, received.Sender))
+        if (!NetworkVerification.HasAuthorityOverPlayer(playerID, sender))
         {
             var descriptor = received.PlatformID.HasValue ? $"{received.PlatformID}" : "with no PlatformID";
-            FusionLogger.Warn($"User {descriptor} attempted to modify metadata for player {data.Player.ID}!");
+            FusionLogger.Warn($"User {descriptor} attempted to modify metadata for player {playerID}!");
+            return;
+        }
+
+        // If the player does not have authority over this specific metadata key, do not allow them to change it
+        if (!PlayerMetadata.Processor.HasAuthorityOverKey(key, sender))
+        {
+            var descriptor = received.PlatformID.HasValue ? $"{received.PlatformID}" : "with no PlatformID";
+            FusionLogger.Warn($"User {descriptor} attempted to modify metadata with key {key}, which they do not have authority over!");
             return;
         }
 
         // Send the response to all clients
-        PlayerSender.SendPlayerMetadataResponse(data.Player.ID, data.Key, data.Value);
+        PlayerSender.SendPlayerMetadataResponse(playerID, key, value);
     }
 }
