a1769afdbda5d3b18c46ae8dd109698ce531fed5
Logan Shockley <loganv182@gmail.com>
2026-05-25 17:28:25 -0500

    Added a bunch of fonts

--- LabFusion/src/UI/Resources/CommonFonts.cs
+++ LabFusion/src/UI/Resources/CommonFonts.cs
@@ -24,6 +24,36 @@ public static class CommonFonts
     public static readonly string BalooBhai2_ExtraBold = "BalooBhai2_ExtraBold";
 
     /// <summary>
+    /// Regular variant of Barriecito.
+    /// The font can be found at <see href="https://fonts.google.com/specimen/Barriecito"/>.
+    /// </summary>
+    public static readonly string Barriecito_Regular = "Barriecito_Regular";
+
+    /// <summary>
+    /// Regular variant of BebasNeue.
+    /// The font can be found at <see href="https://fonts.google.com/specimen/Bebas+Neue"/>.
+    /// </summary>
+    public static readonly string BebasNeue_Regular = "BebasNeue_Regular";
+
+    /// <summary>
+    /// Regular variant of Danfo.
+    /// The font can be found at <see href="https://fonts.google.com/specimen/Danfo"/>.
+    /// </summary>
+    public static readonly string Danfo_Regular = "Danfo_Regular";
+
+    /// <summary>
+    /// Regular variant of Freckle Face.
+    /// The font can be found at <see href="https://fonts.google.com/specimen/Freckle+Face"/>.
+    /// </summary>
+    public static readonly string FreckleFace_Regular = "FreckleFace_Regular";
+
+    /// <summary>
+    /// Regular variant of Henny Penny.
+    /// The font can be found at <see href="https://fonts.google.com/specimen/Henny+Penny"/>.
+    /// </summary>
+    public static readonly string HennyPenny_Regular = "HennyPenny_Regular";
+
+    /// <summary>
     /// Regular variant of Jolly Lodger.
     /// The font can be found at <see href="https://fonts.google.com/specimen/Jolly+Lodger"/>.
     /// </summary>
@@ -40,4 +70,22 @@ public static class CommonFonts
     /// The font can be found at <see href="https://fonts.google.com/specimen/Rubik+Wet+Paint"/>.
     /// </summary>
     public static readonly string RubikWetPaint_Regular = "RubikWetPaint_Regular";
+
+    /// <summary>
+    /// Regular variant of Rye.
+    /// The font can be found at <see href="https://fonts.google.com/specimen/Rye"/>.
+    /// </summary>
+    public static readonly string Rye_Regular = "Rye_Regular";
+
+    /// <summary>
+    /// Regular variant of Special Elite.
+    /// The font can be found at <see href="https://fonts.google.com/specimen/Special+Elite"/>.
+    /// </summary>
+    public static readonly string SpecialElite_Regular = "SpecialElite_Regular";
+
+    /// <summary>
+    /// Regular variant of Uncial Antiqua.
+    /// The font can be found at <see href="https://fonts.google.com/specimen/Uncial+Antiqua"/>.
+    /// </summary>
+    public static readonly string UncialAntiqua_Regular = "UncialAntiqua_Regular";
 }
