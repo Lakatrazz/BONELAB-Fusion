4d844beb8accb0d0d1b3a814b9c214ce586249c5
Logan Shockley <loganv182@gmail.com>
2026-05-25 13:55:41 -0500

    Documented UIDirection

--- LabFusion/src/UI/Elements/UIDirection.cs
+++ LabFusion/src/UI/Elements/UIDirection.cs
@@ -1,12 +1,27 @@
 ﻿namespace LabFusion.UI.Elements;
 
+/// <summary>
+/// Indicates the direction that children are displayed for a UI element.
+/// </summary>
 public enum UIDirection
 {
+    /// <summary>
+    /// Children are displayed from the top down.
+    /// </summary>
     Column,
 
+    /// <summary>
+    /// Children are displayed from the bottom up.
+    /// </summary>
     ColumnReverse,
 
+    /// <summary>
+    /// Children are displayed from left to right.
+    /// </summary>
     Row,
 
+    /// <summary>
+    /// Children are displayed from right to left.
+    /// </summary>
     RowReverse
 }
