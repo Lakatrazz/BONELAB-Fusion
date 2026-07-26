12dcdaa0904d4908e0fb31834fc927163c74e911
Checkerboard <Colehennessy1@gmail.com>
2026-07-26 16:12:58 -0700

    Update version number

--- LabFusion/src/Mod.cs
+++ LabFusion/src/Mod.cs
@@ -48,9 +48,9 @@ public struct FusionVersion
 #else
     public const byte VersionMajor = 1;
     public const byte VersionMinor = 14;
-    public const short VersionPatch = 1;
+    public const short VersionPatch = 2;
 
-    public const string VersionString = "1.14.1";
+    public const string VersionString = "1.14.2";
 #endif
 }
 
